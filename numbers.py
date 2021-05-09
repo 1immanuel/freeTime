dc1d3cb234d255b4d9a3c40a24c5e08940379886
Immanuel Alexander <i.alexander2012@yahoo.com>
2021-05-08 22:14:02 -0500

    adds random number project

---
+++ numbers.py
@@ -0,0 +1,32 @@
+import random
+run = True
+game = True
+chances = 5
+magic_number = random.randint(1, 10)
+
+
+
+print('Welcome. \nGuess a number: ')
+number = int(input())
+
+while game == True:
+    if number != magic_number:
+        chances -= 1
+        number = int(input('Guess again: '))
+    elif number == magic_number:
+        print('Genius!!')
+        game = False
+    else:
+        if chances == 0:
+            game = False
+            print('Better luck next time.')
+else:
+    if game == False:
+        print('Thanks for playing.')
+        break
+
+        
+        
+    
+
+
